722fe05502f5d6017d3c9f3d82d4a09a685bbd6c
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-07-26 22:42:35 +0200

    Major remapping

--- keyboards/ferris/keymaps/zoldar/config.h
+++ keyboards/ferris/keymaps/zoldar/config.h
@@ -38,6 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_ANIMATIONS
--- keyboards/ferris/keymaps/zoldar/keymap.c
+++ keyboards/ferris/keymaps/zoldar/keymap.c
@@ -10,8 +10,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_Q, KC_W, KC_F, KC_P, KC_B,
           KC_J, KC_L, KC_U, KC_Y, KC_SLSH,
 
-          LCMD_T(KC_A), LOPT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G,
-	      KC_M, RSFT_T(KC_N), RCTL_T(KC_E), ROPT_T(KC_I), RCMD_T(KC_O),
+          LCTL_T(KC_A), LOPT_T(KC_R), LCMD_T(KC_S), LSFT_T(KC_T), KC_G,
+	      KC_M, RSFT_T(KC_N), RCMD_T(KC_E), ROPT_T(KC_I), RCTL_T(KC_O),
 
           LT(0,KC_Z), KC_X, KC_C, KC_D, KC_V,
           KC_K, KC_H, KC_COMM, KC_DOT, KC_QUOTE,
@@ -21,26 +21,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
 	[1] = LAYOUT(
           KC_MPLY, KC_MPRV, KC_MNXT, KC_BTN2, KC_WH_U,
-          KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO,
+          LCTL(LOPT(KC_D)), LCTL(LOPT(KC_E)), LCTL(LOPT(KC_C)), LCTL(LOPT(KC_T)), LCTL(LOPT(KC_G)),
 
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_WH_D,
-          KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO,
+          LCTL_T(LCTL(KC_Z)), KC_TRNS, KC_TRNS, KC_BTN1, KC_WH_D,
+          KC_PGUP, KC_HOME, KC_UP, KC_END, LCTL(LOPT(KC_I)),
 
           KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_LSFT,
-          KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_CAPS,
+          KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, LCTL(LOPT(KC_K)),
 
           KC_TRNS, KC_TRNS,
           KC_ESC, KC_TRNS
         ),
 	[2] = LAYOUT(
-          KC_NUBS, KC_LABK, KC_RABK, KC_MINS, KC_PIPE,
+          S(KC_NUBS), KC_LABK, KC_RABK, KC_MINS, KC_PIPE,
           KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR , KC_HASH,
 
-          LCMD_T(KC_EXLM), LOPT_T(KC_ASTR), LCTL_T(KC_SLSH), LSFT_T(KC_EQL), KC_AMPR,
-          S(KC_MINS), RSFT_T(KC_LPRN), RCTL_T(KC_RPRN), ROPT_T(KC_SCLN), RCMD_T(KC_DQUO),
+          LCTL_T(KC_EXLM), LOPT_T(KC_ASTR), LCMD_T(KC_SLSH), LSFT_T(KC_EQL), KC_AMPR,
+          S(KC_MINS), RSFT_T(KC_LPRN), RCMD_T(KC_RPRN), ROPT_T(KC_SCLN), RCTL_T(KC_DQUO),
 
-          S(KC_NUBS), KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC,
-          KC_AT  , KC_COLN, KC_COMM, KC_DOT , KC_QUOT,
+          KC_NUBS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC,
+          KC_AT  , S(KC_SLSH), KC_COMM, KC_DOT , KC_QUOT,
 
           KC_TRNS, S(KC_SCLN),
           KC_TRNS, KC_TRNS
@@ -52,17 +52,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_1, KC_2, KC_3, KC_4, KC_5,
           KC_6, KC_7, KC_8, KC_9, KC_0,
 
-          KC_LCMD, KC_LOPT, KC_LCTL, KC_LSFT, KC_F11,
-          KC_F12, KC_RSFT, KC_RCTL, KC_ROPT, KC_RCMD,
+          KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_F11,
+          KC_F12, KC_RSFT, KC_RCMD, KC_ROPT, KC_RCTL,
 
           KC_TRNS, KC_TRNS,
           KC_TRNS, KC_TRNS
 	)
 };
 
+uint8_t mod_state;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    mod_state = get_mods();
+
     switch (keycode) {
-        case LCMD_T(KC_EXLM):
+        case LCTL(LOPT(KC_I)):
+            if ((mod_state & MOD_MASK_GUI) && record->event.pressed) {
+                tap_code16(LCTL(LOPT(KC_U)));
+		        return false;
+            }
+        case LCTL(LOPT(KC_K)):
+            if ((mod_state & MOD_MASK_GUI) && record->event.pressed) {
+                tap_code16(LCTL(LOPT(KC_J)));
+		        return false;
+            }
+        case LCTL_T(KC_EXLM):
             if (record->tap.count && record->event.pressed) {
                 tap_code16(KC_EXLM);
                 return false;
@@ -74,7 +88,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
-        case LCTL_T(KC_SLSH):
+        case LCMD_T(KC_SLSH):
             if (record->tap.count && record->event.pressed) {
                 tap_code16(KC_SLSH);
                 return false;
@@ -92,7 +106,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
-        case RCTL_T(KC_RPRN):
+        case RCMD_T(KC_RPRN):
             if (record->tap.count && record->event.pressed) {
                 tap_code16(KC_RPRN);
                 return false;
@@ -104,12 +118,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
-        case RCMD_T(KC_DQUO):
+        case RCTL_T(KC_DQUO):
             if (record->tap.count && record->event.pressed) {
                 tap_code16(KC_DQUO);
                 return false;
             }
             break;
+        case LT(0,KC_Z):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_ESC);
+                return false;
+            }
+            return true;
     }
     return true;
 }
