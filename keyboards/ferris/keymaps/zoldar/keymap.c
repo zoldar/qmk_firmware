c3b6399d608a3a233c123e4775c165aca1f7d87a
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-07-30 01:29:36 +0200

    Switch to layout without any mod taps

--- keyboards/ferris/keymaps/zoldar/keymap.c
+++ keyboards/ferris/keymaps/zoldar/keymap.c
@@ -13,11 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_A, KC_R, KC_S, KC_T, KC_G,
 	      KC_M, KC_N, KC_E, KC_I, KC_O,
 
-          LT(0,KC_Z), KC_X, KC_C, KC_D, KC_V,
+          KC_Z, KC_X, KC_C, KC_D, KC_V,
           KC_K, KC_H, KC_COMM, KC_DOT, KC_QUOTE,
 
-          LT(1,KC_TAB), LT(3,KC_SPC),
-          LT(2,KC_ENT), KC_BSPC
+          MO(1), MO(3),
+          KC_SPC, MO(2)
     ),
 	[1] = LAYOUT(
           KC_MPLY, KC_VOLD, KC_VOLU, KC_BTN2, LCMD(KC_RIGHT),
@@ -50,13 +50,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_6, KC_7, KC_8, KC_9, KC_0,
 
           KC_F1, OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LSFT), KC_F11,
-          KC_F12, OSM(MOD_RSFT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_F10,
+          KC_F12, OSM(MOD_RSFT), KC_TAB, KC_NO, KC_F10,
 
           KC_F2, KC_F3, KC_F4, OSM(MOD_LGUI), KC_F5,
-          KC_F6, OSM(MOD_RGUI), KC_F7, KC_F8, KC_F9,
+          KC_F6, KC_ESC, KC_F7, KC_F8, KC_F9,
 
           KC_TRNS, KC_TRNS,
-          KC_ESC, KC_TRNS
+          KC_ENT, KC_BSPC
 	),
 	[4] = LAYOUT(
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -73,28 +73,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-uint8_t mod_state;
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    mod_state = get_mods();
-
-    switch (keycode) {
-        case LCTL(LOPT(KC_I)):
-            if ((mod_state & MOD_MASK_GUI) && record->event.pressed) {
-                tap_code16(LCTL(LOPT(KC_U)));
-		        return false;
-            }
-        case LCTL(LOPT(KC_K)):
-            if ((mod_state & MOD_MASK_GUI) && record->event.pressed) {
-                tap_code16(LCTL(LOPT(KC_J)));
-		        return false;
-            }
-        case LT(0,KC_Z):
-            if (!record->tap.count && record->event.pressed) {
-                tap_code16(KC_ESC);
-                return false;
-            }
-            return true;
-    }
-    return true;
-}
