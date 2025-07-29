88959baee1544c2c0d80ae11837fce72f2973c4b
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-07-29 22:41:06 +0200

    Make significant revamp

--- keyboards/ferris/keymaps/zoldar/keymap.c
+++ keyboards/ferris/keymaps/zoldar/keymap.c
@@ -8,56 +8,69 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
           KC_Q, KC_W, KC_F, KC_P, KC_B,
-          KC_J, KC_L, KC_U, KC_Y, KC_SLSH,
+          KC_J, KC_L, KC_U, KC_Y, KC_NUBS,
 
-          LCTL_T(KC_A), LOPT_T(KC_R), LCMD_T(KC_S), LSFT_T(KC_T), KC_G,
-	      KC_M, RSFT_T(KC_N), RCMD_T(KC_E), ROPT_T(KC_I), RCTL_T(KC_O),
+          KC_A, KC_R, KC_S, KC_T, KC_G,
+	      KC_M, KC_N, KC_E, KC_I, KC_O,
 
           LT(0,KC_Z), KC_X, KC_C, KC_D, KC_V,
           KC_K, KC_H, KC_COMM, KC_DOT, KC_QUOTE,
 
-          LT(3,KC_TAB), LT(1,KC_SPC),
+          LT(1,KC_TAB), LT(3,KC_SPC),
           LT(2,KC_ENT), KC_BSPC
-        ),
+    ),
 	[1] = LAYOUT(
-          KC_MPLY, KC_MPRV, KC_MNXT, KC_BTN2, KC_RIGHT,
+          KC_MPLY, KC_VOLD, KC_VOLU, KC_BTN2, LCMD(KC_RIGHT),
           LCTL(LOPT(KC_D)), LCTL(LOPT(KC_E)), LCTL(LOPT(KC_C)), LCTL(LOPT(KC_T)), LCTL(LOPT(KC_G)),
 
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_LEFT,
+          KC_TRNS, OSM(MOD_LCTL), OSM(MOD_LALT), KC_BTN1, LCMD(KC_LEFT),
           KC_PGUP, KC_HOME, KC_UP, KC_END, LCTL(LOPT(KC_I)),
 
-          LCTL(KC_Z), KC_MUTE, KC_VOLD, KC_VOLU, KC_LSFT,
+          LCTL(KC_Z), KC_MUTE, KC_NO, OSM(MOD_LGUI), KC_NO,
           KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, LCTL(LOPT(KC_K)),
 
           KC_TRNS, KC_TRNS,
-          KC_ESC, KC_TRNS
-        ),
+          MO(4), KC_TRNS
+    ),
 	[2] = LAYOUT(
           S(KC_NUBS), KC_LABK, KC_RABK, KC_MINS, KC_PIPE,
           KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR , KC_HASH,
 
-          LCTL_T(KC_EXLM), LOPT_T(KC_ASTR), LCMD_T(KC_SLSH), LSFT_T(KC_EQL), KC_AMPR,
-          S(KC_MINS), RSFT_T(KC_LPRN), RCMD_T(KC_RPRN), ROPT_T(KC_SCLN), RCTL_T(KC_DQUO),
+          KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL, KC_AMPR,
+          S(KC_MINS), KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO,
 
           KC_NUBS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC,
           KC_AT  , S(KC_SLSH), KC_COMM, KC_DOT , KC_QUOT,
 
           KC_TRNS, S(KC_SCLN),
           KC_TRNS, KC_TRNS
-        ),
+    ),
 	[3] = LAYOUT(
-          KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
-          KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-
           KC_1, KC_2, KC_3, KC_4, KC_5,
           KC_6, KC_7, KC_8, KC_9, KC_0,
 
-          KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_F11,
-          KC_F12, KC_RSFT, KC_RCMD, KC_ROPT, KC_RCTL,
+          KC_F1, OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LSFT), KC_F11,
+          KC_F12, OSM(MOD_RSFT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_F10,
+
+          KC_F2, KC_F3, KC_F4, OSM(MOD_LGUI), KC_F5,
+          KC_F6, OSM(MOD_RGUI), KC_F7, KC_F8, KC_F9,
+
+          KC_TRNS, KC_TRNS,
+          KC_ESC, KC_TRNS
+	),
+	[4] = LAYOUT(
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+          MS_WHLU, MS_WHLL, MS_UP, MS_WHLR, KC_TRNS,
+
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+          MS_WHLD, MS_LEFT, MS_DOWN, MS_RGHT, KC_TRNS,
 
           KC_TRNS, KC_TRNS,
           KC_TRNS, KC_TRNS
-	)
+    ),
 };
 
 uint8_t mod_state;
@@ -76,54 +89,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code16(LCTL(LOPT(KC_J)));
 		        return false;
             }
-        case LCTL_T(KC_EXLM):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_EXLM);
-                return false;
-            }
-            break;
-        case LOPT_T(KC_ASTR):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_ASTR);
-                return false;
-            }
-            break;
-        case LCMD_T(KC_SLSH):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_SLSH);
-                return false;
-            }
-            break;
-        case LSFT_T(KC_EQL):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_EQL);
-                return false;
-            }
-            break;
-        case RSFT_T(KC_LPRN):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_LPRN);
-                return false;
-            }
-            break;
-        case RCMD_T(KC_RPRN):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_RPRN);
-                return false;
-            }
-            break;
-        case ROPT_T(KC_SCLN):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_SCLN);
-                return false;
-            }
-            break;
-        case RCTL_T(KC_DQUO):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_DQUO);
-                return false;
-            }
-            break;
         case LT(0,KC_Z):
             if (!record->tap.count && record->event.pressed) {
                 tap_code16(KC_ESC);
